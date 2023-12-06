aba243a6f20362c2e20e4cda6adf5d0b8844a34f
Sam Walley <swalley2004@gmail.com>
2023-12-05 17:02:57 -0800

    reorganized some lines of code pogchamp

--- src/main/python/combat.py
+++ src/main/python/combat.py
@@ -83,18 +83,16 @@ def loadCombat(transferredData):
     screen = gameData.pygameWindow
     screenX, screenY = screen.get_size()
     enemies = gameData.currentEnemies
-    for character in enemies:
-        character.setCurrentHP(character.health.max_value)
     playerData = gameData.player
     party = playerData.party
     skillsShowing = False
+
+    for character in enemies:
+        character.setCurrentHP(character.health.max_value)
     for character in party:
         character.setCurrentHP(character.health.max_value)
         character.setCurrentMana(character.mana.max_value)
 
-    
-
-    
     visualEntities = []
     buttons = []
     loadJson("combatScreen.json", screenX, screenY, visualEntities, buttons)
